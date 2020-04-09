f4fa27c765c014ab670d332e82aec83a602d11a0
Florian-devweb <courtoux.florian39@gmail.com>
2020-04-09 16:17:30 +0200

    initial commit

---
+++ lib/hash.rb
@@ -0,0 +1,24 @@
+options = { font_size: 10, font_family: "Arial" }
+
+# puts options[:font_size]
+
+noms = ["bob", "billy", "toto"]
+notes = [12, 13, 14]
+
+# puts a.rassoc("two")
+array = [0, 1, 2, 3, 4, 5]
+# i=0
+# noms.each do |var|
+#     puts "item =#{var}"
+#     puts noms[i]
+#     puts notes[i]
+#     i=i+1
+#   end
+
+i=0
+  50.times do |var|
+    puts "var =#{var}"
+    puts i
+    i+=1
+  end
+  
---
+++ lib/test.rb
@@ -0,0 +1,14 @@
+
+tab.each do |var|
+    print_ma_bite(14)
+end
+
+
+# print_toto
+# print_titi
+
+
+# print_ma_bite(toto)
+# print_ma_bite(titi)
+
+    
